084fd337e8760bc431a63c1b3b25d904dac4b686
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-22 17:17:45 +0430

    Test for NaN and string numbers for add function.

--- 01_basics/basic-testing-frontend/src/math.test.js
+++ 01_basics/basic-testing-frontend/src/math.test.js
@@ -9,3 +9,15 @@ it("should add an array of numbers and return the correct sum", () => {
 
   // NOTE: I think we need to test more cases
 });
+
+it("should return NaN, if at least one invalid argument is provided", () => {
+  const arg = ["hi", 1, 2];
+  const result = add(arg);
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const arg = ["1", "2"];
+  const result = add(arg);
+  expect(result).toBe(3);
+});

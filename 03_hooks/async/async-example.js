0e03566a9ead4578bcc0d54b0bd5fb0f98c87278
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-25 13:18:56 +0430

    Make test concurrent

--- 03_hooks/async/async-example.js
+++ 03_hooks/async/async-example.js
@@ -1,12 +1,12 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export function generateToken(userEmail, doneFn) {
-  jwt.sign({ email: userEmail }, 'secret123', doneFn);
+  jwt.sign({ email: userEmail }, "secret123", doneFn);
 }
 
 export function generateTokenPromise(userEmail) {
   const promise = new Promise((resolve, reject) => {
-    jwt.sign({ email: userEmail }, 'secret123', (error, token) => {
+    jwt.sign({ email: userEmail }, "secret123", (error, token) => {
       if (error) {
         reject(error);
       } else {
@@ -17,9 +17,3 @@ export function generateTokenPromise(userEmail) {
 
   return promise;
 }
-
-// generateToken('test@test.com', (err, token) => {
-//   console.log(token);
-// });
-
-// generateTokenPromise('test@test.com').then((token) => console.log(token));

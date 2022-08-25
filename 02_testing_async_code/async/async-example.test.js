b2ba604cc06ee52507c7915c683654e9f8b324a8
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-25 13:00:00 +0430

    Test generateTokenPromise.

--- 02_testing_async_code/async/async-example.test.js
+++ 02_testing_async_code/async/async-example.test.js
@@ -1,5 +1,5 @@
 import { it, expect } from "vitest";
-import { generateToken } from "./async-example";
+import { generateToken, generateTokenPromise } from "./async-example";
 
 it("should generate a valid token", (done) => {
   const email = "user1@gmail.com";
@@ -11,4 +11,16 @@ it("should generate a valid token", (done) => {
       done(err);
     }
   });
+});
+
+it("should generate a valid token asynchronousely", () => {
+  const email = "user1@gmail.com";
+  // NOTE: you should return the promise assertion (when you use .resolve)
+  return expect(generateTokenPromise(email)).resolves.toBeDefined();
+});
+
+it("should generate a valid token asynchronousely", async () => {
+  const email = "user1@gmail.com";
+  const token = await generateTokenPromise(email);
+  expect(token).toBeDefined();
 });

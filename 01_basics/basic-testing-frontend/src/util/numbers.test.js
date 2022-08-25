861310987d86723ae7962474922dd9065d6d7129
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-25 11:28:12 +0430

    Create an integration test for cleanNumbers function.

--- 01_basics/basic-testing-frontend/src/util/numbers.test.js
+++ 01_basics/basic-testing-frontend/src/util/numbers.test.js
@@ -1,33 +1,45 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from "./numbers";
+import { it, expect, describe } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it("should return the passed number back", () => {
-  let result = transformToNumber(-13);
-  const expectedValue = -13;
-  expect(result).toBeTypeOf("number");
-  expect(result).toBe(expectedValue);
-});
-
-it("should convert a simple string number to a number and return it", () => {
-  let result = transformToNumber("-13");
-  const expectedValue = -13;
-  expect(result).toBeTypeOf("number");
-  expect(result).toBe(expectedValue);
-});
-
-it("should return a NaN when an invalid argument is passed as argument", () => {
-  const invalidArgs = [[], "invalid", {}, function () {}];
-  invalidArgs.forEach((arg) => {
-    const result = transformToNumber(arg);
-    const expectedValue = NaN;
+describe("transformToNumber", () => {
+  it("should return the passed number back", () => {
+    let result = transformToNumber(-13);
+    const expectedValue = -13;
     expect(result).toBeTypeOf("number");
     expect(result).toBe(expectedValue);
   });
+
+  it("should convert a simple string number to a number and return it", () => {
+    let result = transformToNumber("-13");
+    const expectedValue = -13;
+    expect(result).toBeTypeOf("number");
+    expect(result).toBe(expectedValue);
+  });
+
+  it("should return a NaN when an invalid argument is passed as argument", () => {
+    const invalidArgs = [[], "invalid", {}, function () {}];
+    invalidArgs.forEach((arg) => {
+      const result = transformToNumber(arg);
+      const expectedValue = NaN;
+      expect(result).toBeTypeOf("number");
+      expect(result).toBe(expectedValue);
+    });
+  });
+
+  it("should throw an error if a symbol is passed as argument", () => {
+    function thrower() {
+      transformToNumber(Symbol.for("hi"));
+    }
+    expect(thrower).toThrow();
+  });
 });
 
-it("should throw an error if a symbol is passed as argument", () => {
-  function thrower() {
-    transformToNumber(Symbol.for("hi"));
-  }
-  expect(thrower).toThrow();
+describe("cleanNumbers", () => {
+  it("should return an array of numbers if an array of string numbers is provided", () => {
+    const numbers = ["1", "2"];
+    const cleanedNumbers = cleanNumbers(numbers);
+    cleanedNumbers.forEach((val) => {
+      expect(val).toBeTypeOf("number");
+    });
+  });
 });

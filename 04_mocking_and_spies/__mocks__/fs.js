2bc73b62f1a336d86f5b36453aa1f61aa9d08159
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-31 16:27:43 +0430

    Add a globally implemented custom mock.

---
+++ 04_mocking_and_spies/__mocks__/fs.js
@@ -0,0 +1,9 @@
+import { vi } from "vitest";
+
+export const promises = {
+  writeFile: vi.fn((_storagePath, _data) => {
+    return new Promise((resolve, _reject) => {
+      resolve();
+    });
+  }),
+}
--- 04_mocking_and_spies/src/util/io.test.js
+++ 04_mocking_and_spies/src/util/io.test.js
@@ -2,22 +2,31 @@ import { promises as fs } from "fs";
 import { it, expect, vi } from "vitest";
 import writeData from "./io"
 
-
-it("should execute the writeFile method", () => {
-  vi.mock("fs");
-  vi.mock("path", () => {
-    return {
-      default: {
-        join(...args) {
-          return args[args.length - 1]; // ignore the path and return the filename
-        }
+vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join(...args) {
+        return args[args.length - 1]; // ignore the path and return the filename
       }
     }
-  });
+  }
+});
+
+it("should execute the writeFile method", () => {
   const data = "test";
   const filename = "test.txt";
   writeData(data, filename);
   expect(fs.writeFile).toBeCalledWith(filename, data);
-  vi.unmock("fs");
-  vi.unmock("path");
 });
+
+
+it("should return a promise that resolves to no value if called correctly", () => {
+  const data = "test";
+  const filename = "test.txt";
+  writeData(data, filename);
+  return expect(fs.writeFile(filename, data)).resolves.toBeUndefined();
+});
+
+vi.unmock("fs");
+vi.unmock("path");

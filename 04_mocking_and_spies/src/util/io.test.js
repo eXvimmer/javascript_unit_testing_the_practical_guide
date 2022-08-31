e7fdc2093cef1b72a4a1f5472df7c136c4d349f7
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-31 16:15:24 +0430

    Mock path.join method.

--- 04_mocking_and_spies/src/util/io.test.js
+++ 04_mocking_and_spies/src/util/io.test.js
@@ -5,10 +5,19 @@ import writeData from "./io"
 
 it("should execute the writeFile method", () => {
   vi.mock("fs");
+  vi.mock("path", () => {
+    return {
+      default: {
+        join(...args) {
+          return args[args.length - 1]; // ignore the path and return the filename
+        }
+      }
+    }
+  });
   const data = "test";
   const filename = "test.txt";
   writeData(data, filename);
-  expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(filename, data);
   vi.unmock("fs");
+  vi.unmock("path");
 });
-

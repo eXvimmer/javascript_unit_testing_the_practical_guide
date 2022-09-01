c3e0a9a30ed316151efed286c83bdb7907b8051e
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-01 12:59:02 +0430

    Write a test to check that the body of a request in the fetch function is converted to JSON.

--- 05_more_mocks/util/http.test.js
+++ 05_more_mocks/util/http.test.js
@@ -3,8 +3,11 @@ import { sendDataRequest } from "./http";
 
 const responseData = { testKey: "testData" };
 
-const testFetch = vi.fn((_url, _data) => {
-  return new Promise((resolve, _reject) => {
+const testFetch = vi.fn((_url, data) => {
+  return new Promise((resolve, reject) => {
+    if (typeof data.body !== "string") {
+      return reject("Not a string.")
+    }
     const testResponse = {
       ok: true,
       json() {
@@ -23,5 +26,16 @@ describe("sendDataRequest", () => {
   it("should return any available response data", () => {
     const test = { test: "test" }
     return expect(sendDataRequest(test)).resolves.toEqual(responseData);
+  });
+
+  it("should return convert the provided data to JSON before sending the request", async () => {
+    const test = { test: "test" }
+    let error;
+    try {
+      await sendDataRequest(test);
+    } catch (err) {
+      error = err;
+    }
+    expect(error).not.toBe("Not a string.");
   });
 });

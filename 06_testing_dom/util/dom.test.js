e499f937d5154fd9d9201ef5d53e88669ff8d248
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-01 15:40:11 +0430

    testing dom functionalities.

--- 06_testing_dom/util/dom.test.js
+++ 06_testing_dom/util/dom.test.js
@@ -1,6 +1,6 @@
 import path from "path";
 import fs from "fs";
-import { describe, expect, it, vi } from "vitest";
+import { expect, it, vi } from "vitest";
 import { Window } from "happy-dom";
 import { showError } from "./dom";
 
@@ -12,3 +12,11 @@ const document = window.document;
 document.write(htmlDocContent);
 
 vi.stubGlobal("document", document);
+
+it("should add an error paragraph to an element with the id of 'errors'", () => {
+  showError("test test");
+  const errorContainerElement = document.getElementById("errors");
+  const errorParagraph = errorContainerElement.firstElementChild;
+
+  expect(errorParagraph).not.toBeNull()
+})
